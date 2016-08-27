806e67b4d487ae3f9bc78a445f1b908e363cec27
Andrew Gursky <agursky.js@gmail.com>
2016-08-27 18:37:16 -0400

    Added Sequence Length Variable

--- js/controllers/mainCtrl.js
+++ js/controllers/mainCtrl.js
@@ -1,7 +1,10 @@
 app.controller('mainCtrl', function($scope) {
     
     var level = 2;
+    var sequenceLength = 2;
     var newNote = Math.ceil(Math.random() * level);
-    console.log(newNote);
+    var sequence = [];
+    
+    
     
 }) 
